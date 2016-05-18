f0cf21e52b26bbe721daf5263126c5db2d5e6b0b
Felicia Weathers <Fel.Weathers@gmail.com>
2016-05-18 00:11:45 -0400

    create EllipticalTrainer and Treadmill struct

--- ExercisType.playground/Contents.swift
+++ ExercisType.playground/Contents.swift
@@ -9,3 +9,16 @@ protocol ExerciseType {
     var caloriesBurned : Double { get }
     var minutes : Double { get }
 }
+
+struct EllipticalTrainer : ExerciseType {
+    let name = "Elliptical Machine"
+    let caloriesBurned : Double
+    let minutes: Double
+}
+
+struct Treadmill : ExerciseType {
+    let name = "Treadmill"
+    let caloriesBurned: Double
+    let minutes: Double
+    let distanceInMiles : Double
+}
